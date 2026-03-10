4100e3cfce68b1852e56a3cb01f80711a75dde1a
Soto <luis.soto41@upr.edu>
2026-03-10 01:25:09 -0400

    Phase1

--- src/Program.cpp
+++ src/Program.cpp
@@ -17,8 +17,8 @@ Program::Program() {
         });
 
     for (int i = 0; i < 30; i++) {
-        float x = 250 + 50 * i;
-        float y = 200 + 50 * i;
+        float x = 250 + 50 * (i % 10);
+        float y = 200 + 50 * (i / 10);
 
         Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
             std::pair<float, float>{x, y}, 
@@ -58,6 +58,11 @@ void Program::Update() {
         for (Projectile& p : Projectile::projectiles) { 
             p.update(); 
 
+            if (p.ID != 0 && HitBox::Collision(player->hitBox, p.getHitBox())) {
+                p.del = true;
+                PlayerReset();
+                break;
+            }
         }
 
         if (lives <= 0 && pauseFrames <= 0) gameOver = true;
@@ -180,6 +185,7 @@ void Program::PlayerReset() {
 
 void Program::Reset() {
     Enemy::enemies.clear();
+    Projectile::projectiles.clear();
     StdEnemy::attackInProgress = false;
     player = new Player((GetScreenWidth() / 2) - 15, GetScreenHeight() * 0.75f);
     respawnCooldown = 1080;
@@ -187,4 +193,24 @@ void Program::Reset() {
     count = 0;
     delay = 0;
     lives = 3;
+
+    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+        std::pair<float, float>{350, 150},
+        new SpEnemy(350, 150)
+    });
+
+    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+        std::pair<float, float>{600, 150},
+        new SpEnemy(600, 150)
+    });
+
+    for (int i = 0; i < 30; i++) {
+        float x = 250 + 50 * (i % 10);
+        float y = 200 + 50 * (i / 10);
+
+        Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+            std::pair<float, float>{x, y},
+            new StdEnemy(x, y)
+        });
+    }
 }
